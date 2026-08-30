0d2af67b9c650d420501c9acd03421bf7cb88c58
Rodrigo Elias <pu2rmm@gmail.com>
2026-08-30 15:21:03 -0300

    Backup: RBCOM Android funcionando no Pixel 7

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,14 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:rbcom/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  testWidgets('RBCOM inicia corretamente', (WidgetTester tester) async {
+    await tester.pumpWidget(const RBCOMApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('RBCOM'), findsWidgets);
+    expect(find.text('Radioamadorismo conectado'), findsOneWidget);
+    expect(find.text('CQER Digital'), findsOneWidget);
+    expect(find.text('Faixas de Radiofrequência'), findsOneWidget);
   });
 }
